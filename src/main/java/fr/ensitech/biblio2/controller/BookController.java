36b2cb3a4c9cad9cffe9b2d8e1110cd12ba32f4b
Kentin Even <kentineven02@gmail.com>
2025-11-04 14:11:27 +0100

    feat: post methode

--- src/main/java/fr/ensitech/biblio2/controller/BookController.java
+++ src/main/java/fr/ensitech/biblio2/controller/BookController.java
@@ -1,8 +1,12 @@
 package fr.ensitech.biblio2.controller;
 
 import fr.ensitech.biblio2.entity.Book;
+import fr.ensitech.biblio2.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,28 +15,53 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController implements IBookController {
 
+  @Autowired
+  private IBookService bookService;
+
   @PostMapping("/create")
   @Override
   public ResponseEntity<Book> createBook(Book book) {
-    return null;
+    if(book == null
+            || book.getIsbn() == null || book.getIsbn().isEmpty()
+            || book.getTitle() == null || book.getTitle().isEmpty()
+            || book.getDescription() == null || book.getDescription().isEmpty()
+            || book.getEditor() == null || book.getEditor().isEmpty()
+            || book.getPublicationDate() == null
+            || book.getCategory() == null || book.getCategory().isEmpty()
+            || book.getLanguage() == null || book.getLanguage().isEmpty()
+            || book.getNbPage() < 0) {
+
+      return new ResponseEntity<Book>(HttpStatus.BAD_REQUEST);
+
+    }
+    try {
+      bookService.addOrUpdateBook(book);
+
+      return new ResponseEntity<>(book, HttpStatus.CREATED);
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @GetMapping("/{id}")
   @Override
-  public ResponseEntity<Book> getBookById(@PathVariable("id") long id) {
+  public ResponseEntity<Book> getBookById(@PathVariable("id") @RequestParam(required = true) long id) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Book> updateBook(Book book) {
+  @PutMapping("/update")
+  public ResponseEntity<Book> updateBook(@RequestBody Book book) {
     return null;
   }
 
   @Override
-  public ResponseEntity<Book> deleteBookById(long id) {
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<Book> deleteBookById(@PathVariable("id") @RequestParam(required = true) long id) {
     return null;
   }
 
+  @GetMapping("/all")
   @Override
   public ResponseEntity<List<Book>> getAllBooks() {
     return null;
